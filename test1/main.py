95c7abfd1a9f3bf400b548675574a9ca7830fe67
xeruvim-xxi <xeruvim-xxi@yandex.ru>
2021-07-22 15:58:00 +0300

    test run kompas

--- test1/main.py
+++ test1/main.py
@@ -1,2 +1,41 @@
-import libs.kAPI5, libs.kAPI7, libs.ksConstants, libs.ksConstants3D
+import os
+import re
+import subprocess
+import pythoncom
+from win32com.client import Dispatch, gencache
 
+# Подключение к API7 программы Компас 3D
+def get_kompas_api7():
+    module = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
+    api = module.IKompasAPIObject(
+        Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(module.IKompasAPIObject.CLSID,
+                                                                 pythoncom.IID_IDispatch))
+    const = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
+    return module, api, const
+
+module7, api7, const7 = get_kompas_api7()   # Подключаемся к API7 
+app7 = api7.Application                     # Получаем основной интерфейс
+app7.Visible = True                         # Показываем окно пользователю (если скрыто)
+app7.HideMessage = const7.ksHideMessageNo   # Отвечаем НЕТ на любые вопросы программы
+print(app7.ApplicationName(FullName=True))  # Печатаем название программы
+
+""" # Функция проверки, запущена-ли программа КОМПАС 3D
+def is_running():
+    proc_list = \
+    subprocess.Popen('tasklist /NH /FI "IMAGENAME eq KOMPAS*"', shell=False, stdout=subprocess.PIPE).communicate()[0]
+    return True if proc_list else False
+
+def start():
+    is_run = is_running()  # True, если программа Компас уже запущена
+
+    module7, api7, const7 = get_kompas_api7()  # Подключаемся к программе
+    app7 = api7.Application  # Получаем основной интерфейс программы
+    app7.Visible = True  # Показываем окно пользователю (если скрыто)
+    app7.HideMessage = const7.ksHideMessageNo  # Отвечаем НЕТ на любые вопросы программы
+
+    app7.MessageBox("Привет!!!")
+
+    if not is_run: app7.Quit()  # Закрываем программу при необходимости
+
+if __name__ == "__main__":    
+    start() """
